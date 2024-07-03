794025ae16b2400df8fd467c6da7b4803dfc9a90
Bryan Young <bryan@intertwinesys.com>
2024-07-03 16:47:34 -0400

    update to universal dl

--- aalc.rb
+++ aalc.rb
@@ -1,8 +1,8 @@
 class Aalc < Formula
   desc "ArtifactAlchemy command-line tool to manage Claude Artifacts"
   homepage "https://github.com/intertwine/artifact-alchemy"
-  url "https://github.com/intertwine/artifact-alchemy/raw/main/releases/download/v0.1.0/aalc-macos.zip"
-  sha256 "1203e800a6e836a57dd8a8ebeeddf35325b4c6d9b5f99261aba1cbf03a969f78"
+  url "https://github.com/intertwine/artifact-alchemy/raw/main/releases/download/v0.1.0/aalc-macos-universal.zip"
+  sha256 "64df6361a6033fff5bfccd8407177cd2ba36d4d1a34e360696d1b6d1076c0f76"
 
   def install
     bin.install "aalc"
